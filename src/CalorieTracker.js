3c73b95ffaff46a45fe018a54171240dd6b71733
zcomstock3 <comstock.zach@gmail.com>
2023-01-05 18:45:09 -0500

    Added functioning AutoComplete box.

--- src/CalorieTracker.js
+++ src/CalorieTracker.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Container from '@mui/material/Container';
 import TextField from '@mui/material/TextField';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -16,6 +16,8 @@ import exercise from './icons/exercise.png';
 function CalorieTracker() {
 
     const [name, setName] = useState("");
+    const [selectedQuery, setSelectedQuery] = useState("");
+    const [foodPreview, setFoodPreview] = useState(null);
     const [amount, setAmount] = useState("");
     const [data, setData] = useState(null);
     const [total, setTotal] = useState(0);
@@ -25,6 +27,7 @@ function CalorieTracker() {
     const [fat, setFat] = useState(0);
     const [numOunces, setNumOunces] = useState(0);
     const [numServing, setNumServing] = useState(0);
+    const [query_results, setQueryResults] = useState([]);
 
     async function fetchAPI() {
         const APP_ID = "7020f5e0";
@@ -37,29 +40,53 @@ function CalorieTracker() {
 
         console.log(data);
         console.log(data.parsed[0].food.nutrients.FAT);
+
+        setNumOunces(amount);
+        const numServings = (1 / 4) * (amount / 1); // desired number of servings
+        const servingSize = data.parsed[0].yield; // serving size in the API response
+        const totalQuantity = numServings * servingSize; // total quantity of the food
+
+        // Extract the desired nutrient data from the API response and multiply it by the number of servings
+        const calories = data.parsed[0].food.nutrients.ENERC_KCAL * numServings;
+        const carbs = data.parsed[0].food.nutrients.CHOCDF * numServings;
+        const protein = data.parsed[0].food.nutrients.PROCNT * numServings;
+        const fat = data.parsed[0].food.nutrients.FAT * numServings;
+
+        setCalories(calories);
+        setCarbs(carbs);
+        setProtein(protein);
+        setFat(fat);
+
+
+        console.log(`Total quantity: ${totalQuantity} servings`);
+        console.log(`Calories: ${calories} kcal`);
+        console.log(`Carbohydrates: ${carbs} g`);
+        console.log(`Protein: ${protein} g`);
+        console.log(`Fat: ${fat} g`);
+    }
+
+    async function foodLookup(query) {
+        const APP_ID = "7020f5e0";
+        const APP_KEY = "132102389cb716fbe45af39b17af701a"
+        const baseURL = `https://api.edamam.com/auto-complete?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+        const response = await fetch(baseURL, { method: 'get', mode: 'cors', headers: {
+            "Content-Type": "application/json"
+        }, });
+        const data = await response.json()
+        console.log(data);
+        setQueryResults(data)
         
-    setNumOunces(amount);
-    const numServings = (1/4) * (amount/1); // desired number of servings
-    const servingSize = data.parsed[0].yield; // serving size in the API response
-    const totalQuantity = numServings * servingSize; // total quantity of the food
+    }
 
-    // Extract the desired nutrient data from the API response and multiply it by the number of servings
-    const calories = data.parsed[0].food.nutrients.ENERC_KCAL * numServings;
-    const carbs = data.parsed[0].food.nutrients.CHOCDF * numServings;
-    const protein = data.parsed[0].food.nutrients.PROCNT * numServings;
-    const fat = data.parsed[0].food.nutrients.FAT * numServings;
-
-    setCalories(calories);
-    setCarbs(carbs);
-    setProtein(protein);
-    setFat(fat);
-    
-
-    console.log(`Total quantity: ${totalQuantity} servings`);
-    console.log(`Calories: ${calories} kcal`);
-    console.log(`Carbohydrates: ${carbs} g`);
-    console.log(`Protein: ${protein} g`);
-    console.log(`Fat: ${fat} g`);
+    async function previewFoodItem(e) {
+        e.preventDefault();
+        const APP_ID = "7020f5e0";
+        const APP_KEY = "132102389cb716fbe45af39b17af701a"
+        const baseURL = `https://api.edamam.com/api/food-database/v2/parser?ingr=${selectedQuery}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+        const response = await fetch(baseURL);
+        const data = await response.json();
+        console.log(data)
+        setFoodPreview(data);
     }
 
 
@@ -73,25 +100,23 @@ function CalorieTracker() {
 
     return (
         <>
-        
-        <CssBaseline />
-        <NavBar/>
-        <div style={{marginTop: '100px', marginBottom: '100px'}}>
-        <Typography style={{letterSpacing: '7px', marginBottom: '20px'}} variant="h1">CalorieCounter</Typography>
-        <img style={{width: '200px', height: '200px'}} src={exercise} alt="fitness icon" />
-        </div>
-        
-        <Container>
-            <MealList setName={setName} setAmount={setAmount} handleSubmit={handleSubmit} name={name} amount={amount} data={data} total={total} calories={calories} carbs={carbs} protein={protein} fat={fat} numOunces={numOunces} />
-            
-            <div>
-            <a style={{fontSize: '9px'}} href="https://www.flaticon.com/free-icons/exercise" title="exercise icons">Exercise icons created by Trazobanana - Flaticon</a>
+
+            <CssBaseline />
+            <NavBar />
+            <div style={{ marginTop: '100px', marginBottom: '100px' }}>
+                <Typography style={{ letterSpacing: '7px', marginBottom: '20px' }} variant="h1">CalorieCounter</Typography>
+                <img style={{ width: '200px', height: '200px' }} src={exercise} alt="fitness icon" />
             </div>
-        </Container>
+
+            <Container>
+                <MealList previewFoodItem={previewFoodItem} foodPreview={foodPreview} setSelectedQuery={setSelectedQuery} setName={setName} queryResults = {query_results} setAmount={setAmount} foodLookup={foodLookup} handleSubmit={handleSubmit} name={name} amount={amount} data={data} total={total} calories={calories} carbs={carbs} protein={protein} fat={fat} numOunces={numOunces} />
+
+
+            </Container>
         </>
 
-    
-        
+
+
     )
 }
 
